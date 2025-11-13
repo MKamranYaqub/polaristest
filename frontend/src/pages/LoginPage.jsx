e4dbe997194b85fa241487e97bf93cb86f0ba2e4
MKamranYaqub <kamran.yaqub@mfsuk.com>
2025-11-13 17:58:48 +0000

    fix: remove vertical centering from auth-container in auth.css - changed from flexbox centering to simple block layout with top alignment

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="auth-container" style={{ marginTop: '1rem' }}>
+    <div className="auth-container" style={{ marginTop: '0rem' }}>
       <div className="slds-box slds-theme_default">
         <h1 className="slds-text-heading_large slds-m-bottom_medium text-align-center">
           Project Polaris Login
